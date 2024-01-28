190ca7e58f70b6b27713ce52e0f20e4a41abb806
Peter Trimmel <ptrimmel@gmail.com>
2024-01-28 20:08:58 +0100

    fixed option handling

--- bashlib.sh
+++ bashlib.sh
@@ -8,14 +8,14 @@ set -o nounset
 set -o pipefail
 set -o errtrace
 
-[[ -v BASHLIB2 ]] || exit 255
+[[ -v BASHLIB ]] || exit 255
 
 declare -r __here__="$( dirname $( readlink -f "$0" ) )"
 declare -r __this__="${0##*/}"
 
 # signal handling
-_sigint( ) { _debug "SIGINT caught!"; exit 127; }
-_sigerr( ) { _debug "SIGERR caught!"; }
+_sigint( ) { _debug "SIGINT ($1) caught!"; exit 127; }
+_sigerr( ) { _debug "SIGERR ($1) caught!"; }
 _sigexit( ) {
     (( _in_getopt == 1 )) && { _is_function _usage && _usage; exit 0; }
     (( $1 == 0 || $1 == 255 )) && exit $1
@@ -26,20 +26,26 @@ _sigexit( ) {
         _debug "    [$i]=${BASH_SOURCE[$i+1]}:${BASH_LINENO[$i]} ${FUNCNAME[$i]}(...)"
     done
 }
-trap '_sigexit $? 1>&2' EXIT
-trap '_sigint $?' INT
-trap '_sigerr $?' ERR
+
+[[ -v NOTRAP ]] || {
+    trap '_sigexit $? 1>&2' EXIT
+    trap '_sigint $?' INT
+    trap '_sigerr $?' ERR
+}
 
 # logging
 _debug( ) { local args="$@"; [[ -v DEBUG ]] && { printf "[%(%T)T] %b %s: %s\n" -1 "\e[94mD\e[0m" "$__this__" "$args" >&2; }; true; }
 _info( )  { local args="$@"; [[ -v QUIET ]] || { printf "[%(%T)T] %b %s: %s\n" -1 "\e[32mI\e[0m" "$__this__" "$args" >&2; }; true; }
-_warn ( ) { local args="$@"; printf "[%(%T)T] %b %s: %s\n" -1 "\e[33mW\e[0m" "$__this__" "$args" >&2; }
+_warn( )  { local args="$@"; printf "[%(%T)T] %b %s: %s\n" -1 "\e[33mW\e[0m" "$__this__" "$args" >&2; }
 _error( ) { local args="$@"; printf "[%(%T)T] %b %s: %s\n" -1 "\007\e[91mE\e[0m" "$__this__" "$args" >&2; }
-_fatal( ) { local args="$@"; printf "[%(%T)T] %b %s: %s\n" -1 "\007\007\007\e[37;41mF\e[0m" "$__this__" "$args" >&2; }
+_fatal( ) { local args="$@"; printf "[%(%T)T] %b %s: %s\n" -1 "\007\007\007\e[37;41mF\e[0m" "$__this__" "$args" >&2; exit 255; }
 
 # predicates
-_is_declared( ) { { [[ -n ${!1+__anything__} ]] || declare -p $1 &>/dev/null; } }
-_is_unset( ) { { [[ -z ${!1+__anything__} ]] && ! declare -p $1 &>/dev/null; } }
+_is_declared( ) { [[ -n ${!1+__anything__} ]] || declare -p $1 &>/dev/null; } 
+_is_unset( ) { [[ -z ${!1+__anything__} ]] && ! declare -p $1 &>/dev/null; }
+_is_set( ) { ! _is_unset $1; }
+_is_empty( ) { [[ -z ${!1} ]]; }
+_is_not_empty( ) { ! _is_empty $1; }
 _is_function( ) { [[ "$(declare -Ff "$1")" ]]; }
 
 declare -i _in_getopt=0
@@ -48,22 +54,32 @@ declare __args__=( "$@" )
 # startup
 __run__( ) {
     _is_declared __options__ && {
-        _in_getopt=1
-        __args__=( $( getopt -an $__this__ $__options__ -- "${__args__[@]}" 2>/dev/null ) )
-        _in_getopt=0
-        eval set -- "${__args__[@]}"
-        while :; do
-            local cbk= arg=
-            case "$1" in
-                --) break ;; 
-                -*) [[ $1 =~ ^[-]+(.+)$ ]]; cbk=_on_opt_${BASH_REMATCH[1]}; arg=; shift;
-                    case "$1" in --) ;; -*) ;; *) arg="$1"; shift ;; esac 
-                    ;;
-                *) _fatal "something wrong in getopt" ;;
-            esac
-            _is_function $cbk && $cbk "$arg"
-        done
-        shift; __args__=( "${@}" )
+        _is_unset __options__[short] || local opt+=" -o ${__options__[short]}" 
+        _is_unset __options__[long] || local opt+=" --long ${__options__[long]}"
+        _is_unset opt || {
+            _in_getopt=1
+            __args__=( $( getopt -n $__this__ $opt -- "${__args__[@]}" 2>/dev/null ) )
+            _in_getopt=0
+            eval set -- "${__args__[@]}"
+            while (( $# )) do
+                opt="$1" arg=
+                case "$opt" in
+                    --) shift; break ;; 
+                    --[a-zA-Z0-9]*) 
+                        [[ $opt =~ ^[-]+(.+)$ ]]; opt=${BASH_REMATCH[1]}
+                        [[ ${__options__[long]} =~ $opt: ]] && { arg="$2"; shift; }
+                        ;;
+                    -[a-zA-Z0-9]*) 
+                        [[ $opt =~ ^[-]+(.+)$ ]]; opt=${BASH_REMATCH[1]}
+                        [[ ${__options__[short]} =~ $opt: ]] && { arg="$2"; shift; }
+                        ;;
+                    *) _fatal "something wrong in getopt" ;;
+                esac
+                shift
+                _is_function _on_options && _on_options "$opt" "$arg"
+            done
+            __args__=( "${@}" )
+        }
     }
     ! _is_function __main__&& _fatal "cannot find '__main__'"
     __main__ "${__args__[@]}" 
---
+++ sample.sh
@@ -0,0 +1,92 @@
+#!/usr/bin/env bash
+#
+BASHLIB="$( dirname $( readlink -f "$0" ) )/bashlib.sh"
+source $BASHLIB || exit 1
+#
+
+# for more details see getopt(1)
+declare -rA __options__=(
+    [short]="Ais::hV" [long]="no-addrconfig,no-idn,service::,help,usage,version"
+)
+
+# text borrowed from 'getent'
+_usage( ) {
+cat <<EOF
+
+Usage: $__this__ [OPTION...] database [key ...]
+Get entries from administrative database.
+
+  -A, --no-addrconfig        do not filter out unsupported IPv4/IPv6 addresses
+                             (with ahosts*)
+  -i, --no-idn               disable IDN encoding
+  -s, --service=CONFIG       Service configuration to be used
+  -h, --help                 Give this help list
+      --usage                Give a short usage message
+  -V, --version              Print program version
+
+Mandatory or optional arguments to long options are also mandatory or optional
+for any corresponding short options.
+
+Supported databases:
+ahosts ahostsv4 ahostsv6 aliases ethers group gshadow hosts initgroups
+netgroup networks passwd protocols rpc services shadow
+EOF
+}
+
+# clear options
+unset NOADDRCONFIG NOIDN SERVICE VERSION
+
+# options callback
+_on_options( ) {
+    local opt="$1" arg="$2"
+    case $opt in
+        A|no-addrconfig) NOADDRCONFIG=yes ;;
+        i|no-idn) NOIDN=yes ;;
+        s|service) _is_not_empty arg && SERVICE="$arg"; true ;;
+        h|help) _usage; exit 0 ;;
+        usage) _usage; exit 0 ;;
+        V|version) VERSION="Version-0.99" ;;
+    esac
+}
+
+# clear parameter
+unset DATABASE KEYS
+
+SUPPORTED=( -
+    ahosts ahostsv4 ahostsv6 aliases ethers group gshadow hosts initgroups
+    netgroup networks passwd protocols rpc services shadow -
+)
+
+##
+##  M A I N
+##
+__main__( ) {
+
+    # print version and ignore the rest
+    _is_set VERSION && { echo $VERSION; exit 0; }
+
+    # set database
+    (( $# > 0 )) || _fatal no database.
+    DATABASE="${1,,}"; shift;
+    [[ ${SUPPORTED[@]} =~ [[:space:]]$DATABASE[[:space:]] ]] || \
+        _fatal database \'$DATABASE\' is not supported.
+
+    # set keys
+    KEYS=( $@ )
+
+    # show options
+    for option in NOADDRCONFIG NOIDN SERVICE; do
+        _is_set $option && echo option $option is set to \'${!option}\'.
+    done
+
+    # show database and keys
+    echo database=\'$DATABASE\'
+    (( ${#KEYS[@]} > 0 )) && { \
+        echo -n keys=\(\  
+        for key in ${KEYS[@]}; do echo -n $key \ ; done
+        echo \)
+    }
+    return 0
+}
+
+__run__ 
